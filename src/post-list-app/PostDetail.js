52bbf85ac8024a28193654ce4fd5c8e08f2dce6c
Binit Chandra Jha <binit@gurzu.com>
2022-07-06 00:24:28 +0545

    feat: PostLists api integration and data displaying

--- src/post-list-app/PostDetail.js
+++ src/post-list-app/PostDetail.js
@@ -1,11 +1,66 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  View,
+  ScrollView,
+} from 'react-native';
+import {api} from '../utils/api';
 
-const PostDetail = () => {
+const styles = StyleSheet.create({
+  content: {
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+});
+
+const PostDetail = ({navigation}) => {
+  const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const postId = navigation.getParam('postId');
+    getPost(postId);
+    getComments(postId);
+  }, [navigation]);
+
+  const getPost = postId => {
+    api(`/posts/${postId}`).then(postDetail => {
+      setPost({postDetail});
+    });
+  };
+
+  const getComments = postId => {
+    api(`/posts/${postId}/comments`).then(commentDetail => {
+      setComments({commentDetail});
+    });
+  };
+
   return (
-    <View>
-      <Text>PostDetail</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={styles.content}>
+        <Text style={styles.title} testID="post-title">
+          {post?.title}
+        </Text>
+        <Text>{post?.body}</Text>
+        <Text style={styles.title}>Comments</Text>
+        <FlatList
+          data={comments}
+          renderItem={({item}) => (
+            <View>
+              <Text>{item.name}</Text>
+            </View>
+          )}
+          keyExtractor={item => item.id.toString()}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- src/post-list-app/PostLists.js
+++ src/post-list-app/PostLists.js
@@ -1,12 +1,78 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+} from 'react-native';
+import {api} from '../utils/api';
 
-const PostLists = () => {
+const styles = StyleSheet.create({
+  row: {
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+  },
+});
+
+// Types
+export const PostRow = ({item, index, onPress}) => (
+  <TouchableOpacity
+    testID={`post-row-${index}`}
+    style={styles.row}
+    onPress={onPress}>
+    <Text>{item.title}</Text>
+  </TouchableOpacity>
+);
+
+const PostList = ({navigation}) => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = () => {
+    api('/posts')
+      .then(postRes => {
+        setPosts(postRes);
+        setLoading(false);
+        setError(null);
+      })
+      .catch(err => {
+        setLoading(false);
+        setError(err?.message);
+      });
+  };
   return (
-    <View>
-      <Text>PostLists</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        testID="post-list"
+        data={posts}
+        renderItem={({item, index}) => (
+          <PostRow
+            item={item}
+            index={index}
+            onPress={() => navigation.navigate('Post', {postId: item.id})}
+          />
+        )}
+        keyExtractor={item => item.id.toString()}
+        ListEmptyComponent={() => {
+          if (loading) {
+            return <Text testID="loading-message">Loading</Text>;
+          }
+
+          if (error) {
+            return <Text testID="error-message">{error}</Text>;
+          }
+
+          return <Text testID="no-results">Sorry, no results found.</Text>;
+        }}
+      />
+    </SafeAreaView>
   );
 };
 
-export default PostLists;
+export default PostList;
